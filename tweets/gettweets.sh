70d837905ee89a4efd5b0bbf45dafac45c7bbf20
Thomas Jaensch <thomasjaensch@gmail.com>
2018-02-21 13:37:29 -0500

    Added gettweets.sh

--- tweets/gettweets.sh
+++ tweets/gettweets.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 exec 2>/dev/null
-$(npm bin)/scrape-twitter timeline realDonaldTrump | head -n 6 > tweets/tweets.json
-sed -i '$ s/.$//' tweets/tweets.json
-echo "]" >> tweets/tweets.json
+/usr/local/bin/scrape-twitter timeline realDonaldTrump | head -n 6 > tweets.json
+sed -i '$ s/.$//' tweets.json
+echo "]" >> tweets.json
