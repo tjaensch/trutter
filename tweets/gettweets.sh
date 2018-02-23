bd70e18a41a9c63067356c4a9de03139a7c7ec8a
Thomas Jaensch <thomasjaensch@gmail.com>
2018-02-23 10:06:49 -0500

    Set up main.go to work with new VM instance

--- tweets/gettweets.sh
+++ tweets/gettweets.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 exec 2>/dev/null
-/usr/local/bin/scrape-twitter timeline realDonaldTrump | head -n 6 > tweets.json
+/usr/bin/scrape-twitter timeline realDonaldTrump | head -n 6 > tweets.json
 sed -i '$ s/.$//' tweets.json
 echo "]" >> tweets.json
+file=`cat tweets.json`
+echo $file | sudo tee /var/www/html/index.html
